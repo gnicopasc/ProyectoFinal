d06f81e88f902d957b76f7a52f40e01affa10f47
Ale Marzano <ajmarzano@gmail.com>
2017-12-13 16:19:49 -0300

    hoja de estilo

---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html class="no-js" lang="">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge"><!--compatibilidad con IE-->
+        <title></title>
+        <meta name="description" content="Mapa de comercios para vegetarianos y veganos">
+        <!--texto en los resúltados de búsqueda-->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!--desactiva el zoomin o zoomout para las vista en móviles. Escala el modelo-->
+        <title>Maisha: Alimentación Alternativa</title>
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="apple-touch-icon" href=#>
+        <!-- Poner el favicon -->
+
+        <link rel="stylesheet" href="css/normalize.css"> <!-- hace que el css se vea igual en todos los navegadores-->
+        <link rel="stylesheet" href="css/main.css">
+    </head>
+    <body>
+        <!--[if lte IE 9]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+        <![endif]--> <!--Avisa que se cambie de navegador-->
+
+        <!-- Add your site or application content here -->
+        <!--Cargar primero el body y después los scripts-->
+        <header>
+    		<div class="logo">
+    			<img src="imagenes/logo.png" alt="Logo" />
+    		</div>
+    		<div class="titular">
+    			<h1 class="titulo">
+    				Maisha: ¿Dónde comemos?
+    			</h1>
+
+
+    		</div>
+    		<div class="user">
+    			<figure class="avatar">
+    				<img src="imagenes/avatar.jpg" alt="" />
+    			</figure>
+    		</div>
+    	</header>
+        <nav>
+    		<ul class="menu">
+    			<li><a href="#">Home</a></li>
+    			<li><a href="#">Registro</a></li>
+    			<li><a href="#">Login</a></li>
+    			<li><a href="#">Alimentación</a></li>
+    			<li><a href="#">Buscar</a></li>
+                <li><a class="publicar" href="#">
+                    <strong>Agregar</strong>
+                </a></li>
+
+    		</ul>
+    	</nav>
+
+
+
+        <!-- Scripts | jQuery -->
+
+        <script src="js/vendor/modernizr-3.5.0.min.js"></script><!--librería de javascrip, compila html5 en los navs-->
+        <script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+
+        <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+        <script>
+            window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+            ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+        </script>
+        <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/estilo', function () {
+    return view('layouts.layout');
+});
 
 Auth::routes();
 
